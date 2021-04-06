503c17cd4e98faf473166e717b45178904336f98
Jon <jonrey3@gmail.com>
2021-04-05 23:15:00 -0400

    add chinese

---
+++ precache-manifest.f587bfb0705e33b5b16488570471d83b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a770f81ad34e7a6b1e359b4fa10ea6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97cc17559bf14e7574c6",
+    "url": "/static/js/2.814a0cc0.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/static/js/2.814a0cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259dfb66fe71950a8c65",
+    "url": "/static/js/main.ee2331bb.chunk.js"
+  },
+  {
+    "revision": "d3a1ee2cb725949044d1",
+    "url": "/static/js/runtime-main.39cd4d72.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.314e4053ccf67c07de65e510334730c7.js"
+  "/precache-manifest.f587bfb0705e33b5b16488570471d83b.js"
 );
 
 self.addEventListener('message', (event) => {
